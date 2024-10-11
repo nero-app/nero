fa609a365c704369fac3584d5332637267601ef0
dacchidocchi <dacchidocchi@gmail.com>
2024-10-10 23:42:47 -0300

    typewind: update to classes derive to support option and vec

--- typewind-macros/src/classes.rs
+++ typewind-macros/src/classes.rs
@@ -1,7 +1,7 @@
 use darling::{FromDeriveInput, FromField, FromMeta};
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput, Fields};
+use syn::{parse_macro_input, Data, DeriveInput, Fields, Type};
 
 #[derive(FromDeriveInput, Default)]
 #[darling(default, attributes(tw))]
@@ -15,6 +15,7 @@ struct FieldOpts {
     skip: bool,
     variant: bool,
 }
+
 enum IncludeBehavior {
     Variant,
     Skip,
@@ -28,6 +29,12 @@ impl FromMeta for IncludeBehavior {
             _ => Err(darling::Error::unknown_value(value)),
         }
     }
+}
+
+enum FieldType {
+    Option,
+    Vec,
+    Other,
 }
 
 pub fn impl_to_classes(input: TokenStream) -> TokenStream {
@@ -50,23 +57,51 @@ pub fn impl_to_classes(input: TokenStream) -> TokenStream {
 
     let class_fields = fields.iter().filter_map(|f| {
         let field_name = &f.ident;
+        let field_ty = &f.ty;
         let opts = FieldOpts::from_field(f).expect("Invalid field options");
-        match !opts.skip && (opts.variant || !matches!(include_behavior, IncludeBehavior::Skip)) {
-            true => Some(quote! { self.#field_name.to_string(), }),
-            false => None,
+
+        if opts.skip || (!opts.variant && matches!(include_behavior, IncludeBehavior::Skip)) {
+            return None;
+        }
+
+        match get_field_type(field_ty) {
+            FieldType::Option => Some(quote! {
+                if let Some(ref value) = self.#field_name {
+                    classes.push(value.to_string());
+                }
+            }),
+            FieldType::Vec => Some(quote! {
+                self.#field_name.iter().for_each(|i| classes.push(i.to_string()));
+            }),
+            FieldType::Other => Some(quote! {
+                classes.push(self.#field_name.to_string());
+            }),
         }
     });
 
     quote! {
         impl #impl_generics ToClasses for #name #ty_generics #where_clause {
             fn classes(&self) -> String {
-                let classes: Vec<String> = vec![
-                    #( #class_fields )*
-                ];
-
+                let mut classes: Vec<String> = Vec::new();
+                #( #class_fields )*
                 classes.join(" ")
             }
         }
     }
     .into()
+}
+
+fn get_field_type(ty: &Type) -> FieldType {
+    match ty {
+        Type::Path(type_path) => type_path
+            .path
+            .segments
+            .first()
+            .map_or(FieldType::Other, |s| match s.ident.to_string().as_str() {
+                "Option" => FieldType::Option,
+                "Vec" => FieldType::Vec,
+                _ => FieldType::Other,
+            }),
+        _ => FieldType::Other,
+    }
 }
