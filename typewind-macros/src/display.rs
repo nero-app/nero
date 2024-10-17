8bdfb8f4cec8ef1ebbf87a706929bd6fa53b71bf
dacchidocchi <dacchidocchi@gmail.com>
2024-10-17 18:54:55 -0300

    typewind: add support for no prefix in display derive

--- typewind-macros/src/display.rs
+++ typewind-macros/src/display.rs
@@ -1,4 +1,4 @@
-use darling::{FromDeriveInput, FromMeta};
+use darling::{FromDeriveInput, FromMeta, FromVariant};
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, Data, DeriveInput, Fields};
@@ -9,6 +9,12 @@ struct DisplayOpts {
     prefix: Option<String>,
     #[darling(multiple)]
     replace: Vec<Replace>,
+}
+
+#[derive(FromVariant, Default)]
+#[darling(default, attributes(display))]
+struct VariantOpts {
+    no_prefix: Option<bool>,
 }
 
 #[derive(FromMeta, Default)]
@@ -23,49 +29,60 @@ pub fn impl_display(input: TokenStream) -> TokenStream {
 
     let name = &input.ident;
     let implementation = match &input.data {
-        Data::Struct(data) => generate_struct_impl(&data.fields),
+        Data::Struct(data) => generate_struct_impl(&data.fields, &opts),
         Data::Enum(data) => generate_enum_impl(data, &opts),
         _ => panic!("Display can only be derived for structs and enums"),
-    };
-
-    let body = match opts.prefix {
-        Some(prefix) => quote! { format!("{}-{}", #prefix, #implementation) },
-        None => implementation,
     };
 
     quote! {
         impl std::fmt::Display for #name {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                write!(f, "{}", #body)
+                write!(f, "{}", #implementation)
             }
         }
     }
     .into()
 }
 
-fn generate_struct_impl(fields: &Fields) -> proc_macro2::TokenStream {
-    match fields {
+fn generate_struct_impl(fields: &Fields, opts: &DisplayOpts) -> proc_macro2::TokenStream {
+    let body = match fields {
         Fields::Unnamed(fields_unnamed) if fields_unnamed.unnamed.len() == 1 => {
             quote! { self.0.to_string() }
         }
         _ => panic!("Display can only be derived for structs with a single unnamed field"),
+    };
+
+    match opts.prefix {
+        Some(ref prefix) => quote! { format!("{}-{}", #prefix, #body) },
+        None => body,
     }
 }
 
 fn generate_enum_impl(data: &syn::DataEnum, opts: &DisplayOpts) -> proc_macro2::TokenStream {
     let match_arms = data.variants.iter().map(|variant| {
         let variant_name = &variant.ident;
+        let variant_opts = VariantOpts::from_variant(variant).expect("Invalid variant options");
+
         let variant_str = opts.replace.iter().fold(
             variant_name.to_string().trim_start_matches('_').to_string(),
-            |acc, replace| acc.replace(&replace.from, &replace.to)
+            |acc, replace| acc.replace(&replace.from, &replace.to),
         );
         let kebab_variant = to_kebab_case(&variant_str);
 
+        let formatted_variant = if variant_opts.no_prefix.unwrap_or_default() {
+            kebab_variant.to_string()
+        } else {
+            match opts.prefix {
+                Some(ref prefix) => format!("{}-{}", prefix, kebab_variant),
+                None => kebab_variant.to_string(),
+            }
+        };
+
         match &variant.fields {
             Fields::Unnamed(_) => quote! {
-                Self::#variant_name(inner) => format!("{}-{}", #kebab_variant, inner)
+                Self::#variant_name(inner) => format!("{}-{}", #formatted_variant, inner)
             },
-            Fields::Unit => quote! { Self::#variant_name => #kebab_variant.to_string() },
+            Fields::Unit => quote! { Self::#variant_name => #formatted_variant.to_string() },
             Fields::Named(_) => panic!("Named fields are not supported in Display derive"),
         }
     });
