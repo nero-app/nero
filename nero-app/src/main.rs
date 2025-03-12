52f88639d19a36ae6b8a3db25bdf452a1dd3ae6f
dacchidocchi <dacchidocchi@gmail.com>
2025-03-12 13:25:27 -0300

    app: add extensions commands

--- nero-app/src/main.rs
+++ nero-app/src/main.rs
@@ -1,14 +1,19 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+mod commands;
+
+use commands::*;
 
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            get_filters,
+            search,
+            get_series_info,
+            get_series_episodes,
+            get_series_videos
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
