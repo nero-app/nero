258f839254d1c6ad4927a8daf18ef3dc71448403
dacchidocchi <dacchidocchi@gmail.com>
2025-03-26 21:29:30 -0300

    app: add basic load of extension

--- nero-app/src/main.rs
+++ nero-app/src/main.rs
@@ -3,9 +3,33 @@
 mod commands;
 
 use commands::*;
+use nero_extensions::{ExtensionManager, WasmExtension};
+use tauri::{async_runtime::block_on, Manager};
+
+const EXTENSIONS_DIR: &str = "Nero";
+
+pub struct AppState {
+    pub extension: WasmExtension,
+}
 
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            let extensions_dir = app.path().document_dir().unwrap().join(EXTENSIONS_DIR);
+            let manager = ExtensionManager::new(extensions_dir)?;
+            // For the moment, load the first extension found in the extensions directory.
+            // TODO: if there are no extensions, open a screen with relevant information for
+            // "how to load an extension".
+            let first_extension = block_on(async {
+                let extensions = manager.get_available_extensions().await?;
+                manager.load_extension_async(&extensions[0].0).await
+            });
+            app.manage(AppState {
+                extension: first_extension?,
+            });
+
+            Ok(())
+        })
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![
             get_filters,
