8f6a7b9db8ea75217936b60a2f980fef2f16201b
dacchidocchi <dacchidocchi@gmail.com>
2025-09-07 02:59:15 -0300

    processors: update wit files and add logging implementation

--- nero-processors/src/lib.rs
+++ nero-processors/src/lib.rs
@@ -102,6 +102,7 @@ impl nero_runtime::WasmComponent for WasmProcessor {
         let mut linker = Linker::new(engine);
         wasmtime_wasi::p2::add_to_linker_async(&mut linker).unwrap();
         wasmtime_wasi_http::add_only_http_to_linker_async(&mut linker).unwrap();
+        wasi_logging_impl::add_to_linker(&mut linker).unwrap();
 
         let processor_pre = match version {
             v if v >= since_v0_1_0_draft::MIN_VER => Ok(ProcessorPre::V0_1_0_DRAFT(
--- nero-processors/src/processors.rs
+++ nero-processors/src/processors.rs
@@ -9,7 +9,8 @@ pub mod since_v0_1_0_draft {
         world: "nero:processor/processor",
         async: true,
         with: {
-            "wasi": wasmtime_wasi::p2::bindings,
+            "wasi:clocks": wasmtime_wasi::p2::bindings::clocks,
+            "wasi:io": wasmtime_wasi::p2::bindings::io,
             "wasi:http": wasmtime_wasi_http::bindings::http,
         },
     });
