045143de0d9258ab7005be4d680a6fa7dedc8ca0
dacchidocchi <dacchidocchi@gmail.com>
2025-09-26 02:49:09 -0300

    processors: add initial ffmpeg-sidecar implementation with stubs

--- nero-processors/src/processors.rs
+++ nero-processors/src/processors.rs
@@ -2,12 +2,15 @@ pub mod since_v0_1_0_draft {
     use nero_runtime::semver::SemanticVersion;
     use wasmtime::component::bindgen;
 
+    use self::nero::processor::ffmpeg_sidecar;
+    use crate::WasmState;
+
     pub const MIN_VER: SemanticVersion = SemanticVersion::new(0, 1, 0);
 
     bindgen!({
         path: "./wit/v0.1.0-draft",
         world: "nero:processor/processor",
-        imports: { default: async },
+        imports: { default: async | trappable },
         exports: { default: async },
         with: {
             "wasi:http": wasmtime_wasi_http::bindings::http,
@@ -15,4 +18,14 @@ pub mod since_v0_1_0_draft {
             "wasi:logging": nero_logging::logging,
         },
     });
+
+    impl ffmpeg_sidecar::Host for WasmState {
+        async fn ffmpeg_path(&mut self) -> wasmtime::Result<Option<String>> {
+            todo!()
+        }
+
+        async fn ffprobe_path(&mut self) -> wasmtime::Result<Option<String>> {
+            todo!()
+        }
+    }
 }
